c51a66b75483c87b1915d8c2480155cf0cf14312
Roman Bienov <rodergstep@gmail.com>
2016-09-19 13:30:49 +0300

    my first download

---
+++ Carusels/js/script.js
@@ -0,0 +1,26 @@
+/*Carusels*/
+$(document).ready(function() {
+   var leftUIEl = $('.carousel-arrow-left');
+    var rightUIEl = $('.carousel-arrow-right');
+    var elementsList = $('.carousel-list');
+
+    var pixelsOffset = 255;
+    var currentLeftValue = 0;
+    var elementsCount = elementsList.find('li').length;
+    var minimumOffset = - ((elementsCount - 3) * pixelsOffset);
+    var maximumOffset = 0;
+ 
+    leftUIEl.click(function() {        
+        if (currentLeftValue != maximumOffset) {
+            currentLeftValue += 255;
+            elementsList.animate({ left : currentLeftValue + "px"}, 500);
+        }        
+    });
+ 
+    rightUIEl.click(function() {        
+        if (currentLeftValue != minimumOffset) {
+            currentLeftValue -= 255;
+            elementsList.animate({ left : currentLeftValue + "px"}, 500);
+        }        
+    });
+});
